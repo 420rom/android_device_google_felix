bd7bb45a1eade300fc96859219dcd571f621585a
abun880007 <abun880007@msn.com>
2023-09-17 16:01:57 +0100

    remove bits for PixelParts

--- device-lineage.mk
+++ device-lineage.mk
@@ -14,10 +14,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
 
-# HBM
-PRODUCT_PACKAGES += \
-    HbmSVManagerOverlayFelix
-
 # Build necessary packages for vendor
 
 # Audio
